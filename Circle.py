3a9f6da262e9849d17fea89d3cc87f672110bb3d
Jisselle <121079290+jisselle@users.noreply.github.com>
2023-03-01 09:51:20 -0800

    Add files via upload

---
+++ Circle.py
@@ -0,0 +1,11 @@
+# Circle.py
+
+
+def c_area(c_radius_a):
+    c_area_calc = float(3.14 * ((c_radius_a) ** 2))
+    return c_area_calc
+
+
+def c_circumference(c_radius_c):
+    c_circumference_calc = float(2 * (3.14 * c_radius_c))
+    return c_circumference_calc
---
+++ Rectangle.py
@@ -0,0 +1,11 @@
+# Rectangle.py
+
+
+def r_area(area_w, area_l):
+    r_area_calc = float(area_w * area_l)
+    return r_area_calc
+
+
+def r_perimeter(p_width, p_length):
+    r_perimeter_calc = float((p_width + p_length) * 2)
+    return r_perimeter_calc
